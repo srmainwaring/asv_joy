6e7253594b5c0c074ea543b40fbd722fa4dd32b7
Rhys Mainwaring <rhys.mainwaring@me.com>
2019-02-07 13:17:47 +0000

    Add acknowledgements and copyright notices.

--- asv_joy_gazebo_plugins/src/TeleopTwistJoyPlugin.cc
+++ asv_joy_gazebo_plugins/src/TeleopTwistJoyPlugin.cc
@@ -13,6 +13,41 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+// Acknowledgments:
+//
+// This code borrows ideas from the following source:
+//
+// 1. Mike Purvis's teleop_twist_joy project:
+// https://github.com/ros-teleop/teleop_twist_joy
+//
+// I have used the same parameter specification as teleop_twist_joy
+// and adapted from the ROS transport layer to Ignition Transport.
+// The closest message in Ignition to geometry_msgs/Twist is   
+// ignition::msgs::CmdVel2D which suits our purpose as a velocity
+// command for simple drive and steer robots.
+//
+
+/**
+  Software License Agreement (BSD)
+  \authors   Mike Purvis <mpurvis@clearpathrobotics.com>
+  \copyright Copyright (c) 2014, Clearpath Robotics, Inc., All rights reserved.
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+  the following conditions are met:
+  * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+    following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+    following disclaimer in the documentation and/or other materials provided with the distribution.
+  * Neither the name of Clearpath Robotics nor the names of its contributors may be used to endorse or promote
+    products derived from this software without specific prior written permission.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WAR-
+  RANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, IN-
+  DIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+  OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "asv_joy_gazebo_plugins/TeleopTwistJoyPlugin.hh"
 
 #include <gazebo/common/common.hh>
